b0b9b73bda9255e56a448a50bcb83e25b7135e89
ashley-perez <ashleypere428@gmail.com>
2023-10-15 17:58:06 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,59 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// canvas to draw on
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 canvas.id = "Canvas";
 app.append(canvas);
+
+const buttonContainer = document.createElement("div");
+buttonContainer.id = "buttonContainer";
+app.append(buttonContainer);
+
+const clearButton = document.createElement("button");
+clearButton.innerText = "clear";
+buttonContainer.append(clearButton);
+
+const canvasContext = canvas.getContext("2d")!;
+let cursorIsMoving = false;
+
+// see if mouse is down
+canvas.addEventListener("mousedown", () => {
+    cursorIsMoving = true;
+    console.log("IN HERE");
+});
+
+// if mouse is up then stop drawing and reset the path
+canvas.addEventListener("mouseup", () => {
+    cursorIsMoving = false;
+    canvasContext.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+// lets actually draw
+function draw(event: MouseEvent) {
+    if (!cursorIsMoving) {
+        return;
+    }
+
+    canvasContext.lineWidth = 2;
+    canvasContext.lineCap = "round";
+
+    canvasContext.lineTo(
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop,
+    );
+    canvasContext.stroke();
+    canvasContext.beginPath();
+    canvasContext.moveTo(
+        event.clientX - canvas.offsetLeft,
+        event.clientY - canvas.offsetTop,
+    );
+}
+
+clearButton.addEventListener("click", () => {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+});
