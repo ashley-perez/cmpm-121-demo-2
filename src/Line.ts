9b7da46b0bf22125777bc5c410f6e0ff17f62741
ashley-perez <ashleypere428@gmail.com>
2023-10-17 20:48:50 -0700

    FIXED step 5 complete???

--- src/Line.ts
+++ src/Line.ts
@@ -1,30 +1,29 @@
 export class Line {
-  private points: { x: number; y: number }[] = [];
-  // private zero = 0;
+    private points: { x: number; y: number }[] = [];
+    // private zero = 0;
 
-  constructor(initialX: number, initialY: number) {
-    this.points.push({ x: initialX, y: initialY });
-  }
-
-  extendLine(x: number, y: number) {
-    this.points.push({ x, y });
-  }
-
-  display(context: CanvasRenderingContext2D) {
-    if (!this.points.length) {
-      return;
+    constructor(initialX: number, initialY: number) {
+        this.points.push({ x: initialX, y: initialY });
     }
 
-    context.beginPath();
-
-    const [firstPoint] = this.points;
-    context.moveTo(firstPoint.x, firstPoint.y);
-
-    context.moveTo(firstPoint.x, firstPoint.y);
-    // context.moveTo(this.points[this.zero].x, this.points[this.zero].y);
-    for (const point of this.points) {
-      context.lineTo(point.x, point.y);
+    extendLine(x: number, y: number) {
+        this.points.push({ x, y });
     }
-    context.stroke();
-  }
+
+    display(context: CanvasRenderingContext2D) {
+        if (!this.points.length) {
+            return;
+        }
+
+        context.beginPath();
+
+        const [firstPoint] = this.points;
+        context.moveTo(firstPoint.x, firstPoint.y);
+
+        // context.moveTo(this.points[this.zero].x, this.points[this.zero].y);
+        for (const point of this.points) {
+            context.lineTo(point.x, point.y);
+        }
+        context.stroke();
+    }
 }
